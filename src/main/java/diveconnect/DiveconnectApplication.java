4a91457e717790236644e67fefc476075fb6580d
mmordoe685 <mmordoe685@g.educaand.es>
2026-02-02 22:32:34 +0100

    Initial commit: Proyecto DiveConnect creado

---
+++ src/main/java/diveconnect/DiveconnectApplication.java
@@ -0,0 +1,13 @@
+package diveconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiveconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/diveconnect/DiveconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package diveconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
